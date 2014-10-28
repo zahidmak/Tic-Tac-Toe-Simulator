65578af90a258b599697b76f282da0c99f7a7254
zahidmak <zahid4688@gmail.com>
2014-10-28 12:39:14 -0400

    Element class added

---
+++ Tic-Tac-Toe-Simulator/Element.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Tic_Tac_Toe_Simulator
+{
+    public class Element : INotifyPropertyChanged
+    {
+        private string _position;
+        private int _value;
+        public string Position
+        {
+            get { return _position; }
+            set { _position = value; NotifyPropertyChanged(); }
+        }
+        public int Value
+        {
+            get { return _value; }
+            set { _value = value; NotifyPropertyChanged(); }
+        }
+        #region INotifyPropertyChanged Members
+
+        public void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+    }
+}
